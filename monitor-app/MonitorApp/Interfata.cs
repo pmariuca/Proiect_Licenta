3d0ac1bb4bdad5461b6bbdac19d9c95fc527a0e9
pmariuca <pricopmariuca21@stud.ase.ro>
2024-04-22 20:01:09 +0300

    removed unused references + added new ones

--- monitor-app/MonitorApp/Interfata.cs
+++ monitor-app/MonitorApp/Interfata.cs
@@ -1,21 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using FirebaseAdmin;
-using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Storage.V1;
 using System.Management;
-using System.Diagnostics;
-using System.Collections;
 
 namespace MonitorAppBackend
 {
